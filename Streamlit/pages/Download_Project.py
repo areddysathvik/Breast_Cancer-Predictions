81d4def2256ed095b7ff975004999b14494ec2bd
sathvik reddy <areddisathvik@gmail.com>
2022-12-26 16:27:59 +0530

    Update Download_Project.py

--- Streamlit/pages/Download_Project.py
+++ Streamlit/pages/Download_Project.py
@@ -2,7 +2,7 @@ import streamlit as st
 import os
 
 st.title('Downlad Full Project')
-with open(r'C:\Users\aredd\OneDrive\Desktop\PythonFiles\Streamlit\pages\BCP.zip','rb') as f:
+with open(r'C:\Users\aredd\OneDrive\Documents\GitHub\Mini_Project-Cancer-Predictions-\Streamlit\pages\BCP.zip','rb') as f:
     c = st.download_button('Download Zip', f, file_name='archive.zip')
 
 if(c):
